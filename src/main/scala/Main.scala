c6630b6a46a6432c2d135a1ce89fae4ff74f0bbf
User <fghfhg@>
2023-10-09 15:20:19 +0300

    done

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,5 @@
 case class Address(city: String, region: String)
 
-class Student(id: Int, name: String, age: Int, address: Address)
-  extends Human(name, age, address)
-  {
-    def displayInfo: String =
-    {
-      s"ID: $id, Name: $name, Age: $age, Address: ${address.city}, ${address.region}, Adult: $isAdult"
-    }
-  }
-
 case class Human(name: String, age: Int, address: Address)
 {
   def isAdult: Boolean = age >= 18
@@ -19,10 +10,6 @@ object Main
 {
   def main(args: Array[String]): Unit =
   {
-
-    val address = Address("City", "Region")
-    val student = new Student(1, "Oleg", 20, address)
-    println(student.displayInfo)
 
     val list = List(4, -8, 1, 9, 10, 3, -6, 18, 2, -5)
 
